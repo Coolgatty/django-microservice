c23c7a6cf38cea7ec409fee1b454d5fb15017ff6
Coolgatty <coolgatty@gmail.com>
2022-05-29 23:22:55 -0400

    port fix

--- main/settings.py
+++ main/settings.py
@@ -17,31 +17,31 @@ PROJECT_DIR = os.path.dirname(__file__)
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # local settings
-if not os.environ.get('CIRCLE_CI_WORKFLOW'):
-    local_settings = import_module('main.local_settings')
+#if not os.environ.get('CIRCLE_CI_WORKFLOW'):
+#    local_settings = import_module('main.local_settings')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-if not os.environ.get('CIRCLE_CI_WORKFLOW'):
-    # SECURITY WARNING: keep the secret key used in production secret!
-    SECRET_KEY = local_settings.SECRET_KEY
-    # SECURITY WARNING: don't run with debug turned on in production!
-    DEBUG = local_settings.DEBUG
-    LOCAL_DATABASES = local_settings.LOCAL_DATABASES
-else:
-    SECRET_KEY = 'secret_circle_ci_key'
-    DEBUG = True
-    LOCAL_DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'django_ci',
-            'USER': 'postgres',
-            'PASSWORD': 'postgres',
-            'HOST': '127.0.0.1',
-            'PORT': 5432,
-        },
-    }
+# if not os.environ.get('CIRCLE_CI_WORKFLOW'):
+#     # SECURITY WARNING: keep the secret key used in production secret!
+#     SECRET_KEY = local_settings.SECRET_KEY
+#     # SECURITY WARNING: don't run with debug turned on in production!
+#     DEBUG = local_settings.DEBUG
+#     LOCAL_DATABASES = local_settings.LOCAL_DATABASES
+# else:
+SECRET_KEY = 'secret_circle_ci_key'
+DEBUG = True
+LOCAL_DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    },
+}
 
 ALLOWED_HOSTS = []
 
