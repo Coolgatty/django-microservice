722123d5cb6f0fdcdd28895a73ea4faafc561ab8
dtmaltrain22 <daniel.maltrain@vestua.com>
2022-05-29 23:17:35 -0400

    removed db added ngix

--- main/settings.py
+++ main/settings.py
@@ -122,7 +122,7 @@ DATABASES = {
     #         'PORT': 5432,
     #     }
 }
-DATABASES.update(LOCAL_DATABASES)
+# DATABASES.update(LOCAL_DATABASES)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
